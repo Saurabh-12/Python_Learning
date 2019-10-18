cefb370069dc591c6b2aaaf2c44aa969f6223a86
Saurabh-12 <saurabh.sharma569@gmail.com>
2019-10-18 22:56:01 +0530

    String Manipulation exmple

---
+++ StringManipulation.py
@@ -0,0 +1,14 @@
+my_string = "Hi Saurabh !"
+print(my_string)
+print(my_string*2)
+print(my_string+"2")
+print(my_string+"I love python "*2)
+
+# reverse the string 
+print(my_string[::-1])
+my_list = [1,3,5,7]
+print(my_list[::-1])
+
+# Word list 
+my_word = ["word", "python", "is", "this"]
+print(" ".join(my_word[::-1]))
